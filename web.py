b77872315f54c3caec0a42f67c34ce8431b82db5
Carl00291850 <00291850@student.necc.edu>
2019-12-09 12:11:48 -0500

    first commit

--- web.py
+++ web.py
@@ -1,11 +1,11 @@
-#import libraies for wen scrapping
+#imports libraies for web scraping
 import requests
 from bs4 import BeautifulSoup
 #request the data form the website
 page = requests.get('https://www.newegg.com/p/pl?Submit=StoreIM&Depa=1&Category=20&N=600000022%20600000280%2050001312%2050001314%2050001315%2050001944')
 #load data to  bs4 and put it into a variable where beutufullsoup can understand
 soup = BeautifulSoup(page.content, 'html.parser')
-#get the first item of the web page to start webscrapping
+#get the first item of the web page to start web scraping
 containers = soup.find_all('div',class_='item-container')
 #make a for loop of the first item to grab  the remaining item to get
 for container in containers:
@@ -19,7 +19,7 @@ for container in containers:
     Shipping_name = Shipping_container[0].text.strip()
 
     Price_container = container.find_all('li',class_='price-current')
-    price_name = Price_container[0].text.strip('|\n\s')
+    price_name = Price_container[0].text.strip('|\n')
 
 #print the items of thr wen page
     print(brand_title)
